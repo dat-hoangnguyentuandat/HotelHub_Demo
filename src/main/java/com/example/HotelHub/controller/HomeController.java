0c66126e50781f4e6d0b2eb030d769915854ab29
tuandat <hoangdatlnbp@gmail.com>
2026-02-07 15:20:41 +0700

    feat: add login feature

--- src/main/java/com/example/HotelHub/controller/HomeController.java
+++ src/main/java/com/example/HotelHub/controller/HomeController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller public class HomeController {
     @GetMapping("/register")
     public String hello(Model model) {
-        return "register/register.html";
+        return "register/register";
     }
 
     @GetMapping("/customer_mode")
     public String customer_mode(Model model) {
         return "customer_mode/customer_mode";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login/login";
     }
 }
